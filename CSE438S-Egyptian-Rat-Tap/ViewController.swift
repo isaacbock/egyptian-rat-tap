6e132021f89b3cce9ec24159fbf359ea02365926
Isaac Bock <isaactbock@wustl.edu>
2020-11-14 23:00:19 -0600

    Initial Commit

---
+++ CSE438S-Egyptian-Rat-Tap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CSE438S-Egyptian-Rat-Tap
+//
+//  Created by Isaac Bock on 11/14/20.
+//  Copyright © 2020 Egyptian Rat Tap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
