b90d2e84e01f8046c7df521297cf1bac7ef8decf
Isabel Sangimino <isangimino@wustl.edu>
2020-11-20 19:04:12 -0600

    added card and deck structs

---
+++ CSE438S-Egyptian-Rat-Tap/Gameplay/Deck.swift
@@ -0,0 +1,9 @@
+//
+//  Deck.swift
+//  CSE438S-Egyptian-Rat-Tap
+//
+//  Created by Isabel Sangimino on 11/20/20.
+//  Copyright © 2020 Egyptian Rat Tap. All rights reserved.
+//
+
+import Foundation
